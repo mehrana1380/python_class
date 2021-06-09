387b0b7239a02fa95a577fab2bfe27046e98ee5f
mehrana1380 <84845533+mehrana1380@users.noreply.github.com>
2021-06-09 02:43:28 -0700

    Add files via upload

---
+++ python_3_2.py
@@ -0,0 +1,22 @@
+def mashin_hesab(number_one, number_two):
+    if amaliat == '+':
+
+            print(number_one + number_two)
+    elif amaliat == '-':
+
+            print(number_one - number_two)
+    elif amaliat == '*':
+
+            print(number_one * number_two)
+    elif amaliat == '%' :
+
+            print(number_one % number_two)
+    elif amaliat == '**'
+            print(number_one ** number_two)
+
+            
+number_one = float(input("enter your first num: "))
+number_two = float(input("enter your second num: "))
+amaliat = input("nov amaliat ra moshakhas konid")
+
+mashin_hesab(number_one, number_two)
